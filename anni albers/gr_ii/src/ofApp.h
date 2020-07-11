fdad7cb4cd9dbfd2a2aaef4fc18d1ecd0b437538
Sasha Paulovich <Sasha.Paulovich@renaissance.com>
2020-07-10 22:46:44 -0400

    anni albers: gr ii

---
+++ anni albers/gr_ii/src/ofApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		void drawBasicSquare(ofColor color);
+		void drawChevron(ofColor chevronColor);
+		void drawHorizontalChevron(ofColor chevronColor);
+		void flipXAxis();
+		void flipYAxis();
+
+		string appName;
+		bool saveScreen;
+		int innerWidth, innerHeight, numSquares;
+		float xPadding, yPadding, length;
+		
+		ofColor cream, red, blue;
+};
